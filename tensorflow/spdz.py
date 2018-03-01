e1d84719c1bb05d5d02a371dcf2b51ec26f25871
Morten Dahl <mortendahlcs@gmail.com>
2018-03-01 23:26:53 +0100

    ..

--- tensorflow/spdz.py
+++ tensorflow/spdz.py
@@ -1,14 +1,11 @@
+#!/usr/bin/env python2
 
 # TODOs:
-# - recombine without blowing up numbers (should fit in 64bit word)
-# - fixedpoint encoding
 # - truncation operation (using CRT mod operation)
-# - dot product
-# - gradient computation
-# - SDG loop
+# - recombine without blowing up numbers (should fit in 64bit word)
+# - gradient computation + SGD
 # - compare performance if native type is float64 instead of int64
 # - performance on GPU
-# - recombine without blowing up numbers
 
 from datetime import datetime
 from functools import reduce
@@ -24,34 +21,16 @@ prod = lambda xs: reduce(lambda x,y: x*y, xs)
 # Idea is to simulate five different players on different devices. 
 # Hopefully Tensorflow can take care of (optimising?) networking like this.
 
-SERVER_0 = '/device:CPU:0'
-SERVER_1 = '/device:CPU:1'
-CRYPTO_PRODUCER = '/device:CPU:2'
-INPUT_PROVIDER  = '/device:CPU:3'
-OUTPUT_RECEIVER = '/device:CPU:4'
-
-config = tf.ConfigProto(
-    log_device_placement=True,
-    device_count={"CPU": 5},
-    inter_op_parallelism_threads=1,
-    intra_op_parallelism_threads=1
-)
-
-DOT_OP = tf.matmul
-
-# NATIVE_TYPE = tf.float64 # TODO didn't work out of the box
-
-
 #
 # 64 bit CRT
-# - 5 components for modulus ~120 bits for encoding 16.32
+# - 5 components for modulus ~120 bits (encoding 16.32)
 #
-
+# BITPRECISION_INTEGRAL   = 16
+# BITPRECISION_FRACTIONAL = 32
 # NATIVE_TYPE = tf.int64
+# TRUNCATION_GAP = 20
 # m = [89702869, 78489023, 69973811, 70736797, 79637461]
 # M = 2775323292128270996149412858586749843569 # == prod(m)
-# assert log2(M) >= 120
-# for mi in m: assert 2*log2(mi) + log2(1024) < 63
 # lambdas = [
 #     875825745388370376486957843033325692983, 
 #     2472444909335399274510299476273538963924, 
@@ -62,15 +41,16 @@ DOT_OP = tf.matmul
 
 #
 # 32 bit CRT
-# - 10 components for modulus ~100 bits for encoding 10.22
 # - we need this to do dot product as int32 is the only supported type for that
+# - tried tf.float64 but didn't work out of the box
+# - 10 components for modulus ~100 bits
 #
-
+BITPRECISION_INTEGRAL   = 16
+BITPRECISION_FRACTIONAL = 16
 NATIVE_TYPE = tf.int32
+TRUNCATION_GAP = 20
 m = [1201, 1433, 1217, 1237, 1321, 1103, 1129, 1367, 1093, 1039]
 M = 6616464272061971915798970247351 # == prod(m)
-assert log2(M) >= 100
-for mi in m: assert 2*log2(mi) + log2(1024) < 31
 lambdas = [
     1008170659273389559193348505633, 
     678730110253391396805616626909, 
@@ -84,6 +64,33 @@ lambdas = [
     3107636732210050331963327700392
 ]
 
+for mi in m: assert 2*log2(mi) + log2(1024) < log2(NATIVE_TYPE.max)
+assert log2(M) >= 2 * (BITPRECISION_INTEGRAL + BITPRECISION_FRACTIONAL) + log2(1024) + TRUNCATION_GAP
+
+
+SERVER_0 = '/device:CPU:0'
+SERVER_1 = '/device:CPU:1'
+CRYPTO_PRODUCER = '/device:CPU:2'
+INPUT_PROVIDER  = '/device:CPU:3'
+OUTPUT_RECEIVER = '/device:CPU:4'
+
+config = tf.ConfigProto(
+    log_device_placement=True,
+    device_count={"CPU": 5},
+    inter_op_parallelism_threads=1,
+    intra_op_parallelism_threads=1
+)
+
+TENSORBOARD_DIR = '/tmp/tensorflow'
+DOT_OP = tf.matmul
+
+
+def encode(rationals, precision=BITPRECISION_FRACTIONAL):
+    return (rationals * (2**precision)).astype(int).astype(object) % M
+
+def decode(elements, precision=BITPRECISION_FRACTIONAL):
+    map_negative_range = np.vectorize(lambda element: element if element <= M/2 else element - M)
+    return map_negative_range(elements).astype(float) / (2**precision)
 
 def decompose(x):
     return tuple( x % mi for mi in m )
@@ -306,26 +313,32 @@ input_x, x = define_input((10,10))
 input_w, w = define_input((10,10))
 input_b, b = define_input((10,10))
 
+# TODO: with truncation operation before adding b
+# (and then we don't need double precision for B)
+
 # Computation
-z = reveal(dot(x, w) + b)
+y = dot(x, w) + b
+z = reveal(y)
 
 # Actual inputs
-X = np.array([2**80 for _ in range(10*10)]).reshape((10,10)) % M
-W = np.array([    2 for _ in range(10*10)]).reshape((10,10)) % M
-B = np.array([    0 for _ in range(10*10)]).reshape((10,10)) % M
-
+# X = np.array([2**80 for _ in range(10*10)]).reshape((10,10))
+# W = np.array([    2 for _ in range(10*10)]).reshape((10,10))
+# B = np.array([    0 for _ in range(10*10)]).reshape((10,10))
+X = np.random.randn(10,10)
+W = np.random.randn(10,10)
+B = np.random.randn(10,10)
 
 # Decomposed values outside Tensorflow
 inputs = dict(
-    [ (xi, Xi) for xi, Xi in zip(input_x, decompose(X)) ] +
-    [ (wi, Wi) for wi, Wi in zip(input_w, decompose(W)) ] +
-    [ (bi, Bi) for bi, Bi in zip(input_b, decompose(B)) ]
+    [ (xi, Xi) for xi, Xi in zip(input_x, decompose(encode(X))) ] +
+    [ (wi, Wi) for wi, Wi in zip(input_w, decompose(encode(W))) ] +
+    [ (bi, Bi) for bi, Bi in zip(input_b, decompose(encode(B, precision=2*BITPRECISION_FRACTIONAL))) ]
 )
 
 # Run computation using Tensorflow
 with tf.Session(config=config) as sess:
 
-    writer = tf.summary.FileWriter("/tmp/tensorflow", sess.graph)
+    writer = tf.summary.FileWriter(TENSORBOARD_DIR, sess.graph)
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
     run_metadata = tf.RunMetadata()
 
@@ -341,8 +354,7 @@ with tf.Session(config=config) as sess:
     writer.add_run_metadata(run_metadata, '')
     writer.close()
 
-# Recombine result outside Tensorflow
-Z = recombine(res)
+# Recover result outside Tensorflow
+Z = decode(recombine(res), precision=2*BITPRECISION_FRACTIONAL)
 
-
-assert (Z == (np.dot(X, W) + B)).all(), Z
+assert (Z - (np.dot(X, W) + B) < 1e-3).all()
