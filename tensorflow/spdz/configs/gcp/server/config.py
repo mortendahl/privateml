79269ab5f6b2201b52046a2c2c8552870e2c5188
Morten Dahl <mortendahlcs@gmail.com>
2018-03-30 00:42:46 +0200

    ..

--- tensorflow/spdz/configs/gcp/server/config.py
+++ tensorflow/spdz/configs/gcp/server/config.py
@@ -17,11 +17,11 @@ JOB_NAME = 'spdz'
 # INPUT_PROVIDER  = '/job:{}/task:3'.format(JOB_NAME)
 # OUTPUT_RECEIVER = '/job:{}/task:3'.format(JOB_NAME)
 
-SERVER_0 = '/job:{}/replica:0/task:0'.format(JOB_NAME)
-SERVER_1 = '/job:{}/replica:0/task:1'.format(JOB_NAME)
-CRYPTO_PRODUCER = '/job:{}/replica:0/task:2'.format(JOB_NAME)
-INPUT_PROVIDER  = '/job:{}/replica:0/task:3'.format(JOB_NAME)
-OUTPUT_RECEIVER = '/job:{}/replica:0/task:3'.format(JOB_NAME)
+SERVER_0 = '/job:{}/replica:0/task:0/cpu:0'.format(JOB_NAME)
+SERVER_1 = '/job:{}/replica:0/task:1/cpu:0'.format(JOB_NAME)
+CRYPTO_PRODUCER = '/job:{}/replica:0/task:2/cpu:0'.format(JOB_NAME)
+INPUT_PROVIDER  = '/job:{}/replica:0/task:3/cpu:0'.format(JOB_NAME)
+OUTPUT_RECEIVER = '/job:{}/replica:0/task:3/cpu:0'.format(JOB_NAME)
 
 with open(HOSTS_FILE, 'r') as f:
     HOSTS = json.load(f)
